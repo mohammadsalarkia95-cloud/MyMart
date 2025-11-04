ce96b37cf38ad07194068563a7b813ce520d046e
mohammadsalarkia95-cloud <mohammad.salarkia95@gmail.com>
2025-11-04 15:34:16 +0330

    Add PWA meta + manifest links; SSG product page

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,29 @@
-import "../styles/globals.css"
-import TopNav from "../components/TopNav"
-export default function App({Component,pageProps}){ return (<div className="min-h-screen"><TopNav/><Component {...pageProps}/></div>) }
+import '../styles/globals.css'
+import TopNav from '../components/TopNav'
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2B7A78" />
+
+        {/* Icon (PNG هم قبول است) */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/logo.png" />
+
+        {/* Meta پایه */}
+        <meta name="application-name" content="MyMart" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+
+      <div className="min-h-screen">
+        <TopNav />
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
+}
