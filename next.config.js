ee325c4f555c6881cb114b0bf1a51849b8a7720a
mohammadsalarkia95-cloud <mohammad.salarkia95@gmail.com>
2025-11-04 15:07:28 +0330

    remove next export for vercel (enable SSR)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,5 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] },
-  output: "export"
 }
 module.exports = nextConfig
